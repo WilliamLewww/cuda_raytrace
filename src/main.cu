f4eba2d614f231aafd16835e89520ee99fd7b5b6
William Lew <WilliamMilesLew@gmail.com>
2019-10-26 20:51:08 -0700

    added: constant definitions for image dimensions

--- src/main.cu
+++ src/main.cu
@@ -1,9 +1,13 @@
 #include <fstream>
+#define IMAGE_WIDTH 100
+#define IMAGE_HEIGHT 100
 
 int main(void) {
 	std::ofstream file;
 	file.open("image.ppm");
-	file << "P3\n" << 100 << " " << 100 << "\n255\n";
+	file << "P3\n" << IMAGE_WIDTH << " " << IMAGE_HEIGHT << "\n255\n";
+
+	file.close();
 
 	return 0;
 }
