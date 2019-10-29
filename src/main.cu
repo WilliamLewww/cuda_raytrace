2e7708924265f80724f195feba09bc6aaf1e4e23
William Lew <WilliamMilesLew@gmail.com>
2019-10-28 19:04:35 -0700

    added: memory management for colorFromRay kernel

--- src/main.cu
+++ src/main.cu
@@ -4,6 +4,16 @@
 #define IMAGE_WIDTH 100
 #define IMAGE_HEIGHT 100
 
+__global__
+void colorFromRay(Tuple* colorData) {
+	int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
+	int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
+
+	if (idx < IMAGE_WIDTH && idy < IMAGE_HEIGHT) {
+
+	}
+}
+
 void writeColorDataToFile(Tuple* colorData) {
 	std::ofstream file;
 	file.open("image.ppm");
@@ -20,5 +30,18 @@ void writeColorDataToFile(Tuple* colorData) {
 }
 
 int main(void) {
+	Tuple* h_colorData = (Tuple*)malloc(IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple));
+	Tuple* d_colorData;
+	cudaMalloc((Tuple**)&d_colorData, IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple));
+
+	dim3 block(32, 32);
+	dim3 grid((IMAGE_WIDTH + block.x - 1) / block.x, (IMAGE_HEIGHT + block.y - 1) / block.y);
+	colorFromRay<<<grid, block>>>(d_colorData);
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(h_colorData, d_colorData, IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple), cudaMemcpyDeviceToHost);
+	cudaFree(d_colorData);
+	
+	free(h_colorData);
 	return 0;
 }
