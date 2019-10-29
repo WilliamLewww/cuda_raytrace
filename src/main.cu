d3fe05ddd6bed73882cd3fd45f04d8cc46ca7aa0
William Lew <WilliamMilesLew@gmail.com>
2019-10-29 12:20:03 -0700

    added: more analysis locations

--- src/main.cu
+++ src/main.cu
@@ -4,8 +4,8 @@
 #include "shape.h"
 #include "ray.h"
 
-#define IMAGE_WIDTH 100
-#define IMAGE_HEIGHT 100
+#define IMAGE_WIDTH 1000
+#define IMAGE_HEIGHT 1000
 
 __device__
 bool intersectSphere(Sphere sphere, Ray ray) {
@@ -59,12 +59,16 @@ int main(void) {
 	printf("\n");
 
 	Analysis::setAbsoluteStart();
-	Analysis::createLabel(0, "kernel");
-	Analysis::createLabel(1, "create_image");
+	Analysis::createLabel(0, "allocate_memory");
+	Analysis::createLabel(1, "execute_kernel");
+	Analysis::createLabel(2, "copy_device");
+	Analysis::createLabel(3, "create_image");
 
+	Analysis::begin();
 	Tuple* h_colorData = (Tuple*)malloc(IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple));
 	Tuple* d_colorData;
 	cudaMalloc((Tuple**)&d_colorData, IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple));
+	Analysis::end(0);
 
 	dim3 block(32, 32);
 	dim3 grid((IMAGE_WIDTH + block.x - 1) / block.x, (IMAGE_HEIGHT + block.y - 1) / block.y);
@@ -74,16 +78,18 @@ int main(void) {
 	colorFromRay<<<grid, block>>>(d_colorData);
 	cudaDeviceSynchronize();
 	printf("finished rendering\n");
-	Analysis::end(0);
+	Analysis::end(1);
 
+	Analysis::begin();
 	cudaMemcpy(h_colorData, d_colorData, IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple), cudaMemcpyDeviceToHost);
 	cudaFree(d_colorData);
+	Analysis::end(2);
 
 	Analysis::begin();
 	const char* filename = "image.ppm";
 	writeColorDataToFile(filename, h_colorData);
 	printf("saved image as: [%s]\n", filename);
-	Analysis::end(1);
+	Analysis::end(3);
 
 	Analysis::printAll(IMAGE_WIDTH, IMAGE_HEIGHT);
 
