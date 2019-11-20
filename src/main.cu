01138a5ebced9a3ed30976d77900b4e96ad0a578
William Lew <WilliamMilesLew@gmail.com>
2019-11-20 15:25:45 -0800

    created: foundation for new test file

--- src/main.cu
+++ src/main.cu
@@ -11,6 +11,14 @@ void colorFromRay(Tuple* colorOut) {
 	int idy = (blockIdx.y * blockDim.y) + threadIdx.y;
 
 	if (idx >= IMAGE_WIDTH || idy >= IMAGE_HEIGHT) { return; }
+
+	Tuple origin = {0.0f, 0.0f, 0.0f, 1.0f};
+	Tuple pixel = {idx - (IMAGE_WIDTH / 2.0f), idy - (IMAGE_HEIGHT / 2.0f), 100.0f, 1.0f};
+	Tuple direction = normalize(pixel - origin);
+
+	Ray ray = {origin, direction};
+
+	colorOut[(idy*IMAGE_WIDTH)+idx] = {0.0f, 0.0f, 0.0f, 1.0f};
 }
 
 void writeColorDataToFile(const char* filename, Tuple* colorData) {
@@ -28,5 +36,27 @@ void writeColorDataToFile(const char* filename, Tuple* colorData) {
 }
 
 int main(int argn, char** argv) {
+	printf("\n");
+
+	Tuple* h_colorData = (Tuple*)malloc(IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple));
+	Tuple* d_colorData;
+	cudaMalloc((Tuple**)&d_colorData, IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple));
+
+	dim3 block(32, 32);
+	dim3 grid((IMAGE_WIDTH + block.x - 1) / block.x, (IMAGE_HEIGHT + block.y - 1) / block.y);
+
+	colorFromRay<<<grid, block>>>(d_colorData);
+	cudaDeviceSynchronize();
+
+	cudaMemcpy(h_colorData, d_colorData, IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple), cudaMemcpyDeviceToHost);
+	cudaFree(d_colorData);
+
+	const char* filename = argv[1];
+	writeColorDataToFile(filename, h_colorData);
+	printf("saved image as: [%s]\n", filename);
+
+	cudaDeviceReset();
+	free(h_colorData);
+	printf("\n");
 	return 0;
 }
