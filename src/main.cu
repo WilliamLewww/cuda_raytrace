43ecc663a9a785fd6bb5e785f6178aa931b4f802
William Lew <WilliamMilesLew@gmail.com>
2019-11-20 23:27:38 -0800

    added: analysis output

--- src/main.cu
+++ src/main.cu
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <stdio.h>
 #include "structures.h"
+#include "analysis.h"
 
 #define IMAGE_WIDTH 1000
 #define IMAGE_HEIGHT 1000
@@ -95,6 +96,13 @@ void writeColorDataToFile(const char* filename, Tuple* colorData) {
 int main(int argn, char** argv) {
 	printf("\n");
 
+	Analysis::setAbsoluteStart();
+	Analysis::createLabel(0, "allocate_memory");
+	Analysis::createLabel(1, "execute_kernel");
+	Analysis::createLabel(2, "copy_device");
+	Analysis::createLabel(3, "create_image");
+
+	Analysis::begin();
 	Tuple* h_colorData = (Tuple*)malloc(IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple));
 	Tuple* d_colorData;
 	cudaMalloc((Tuple**)&d_colorData, IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple));
@@ -107,19 +115,30 @@ int main(int argn, char** argv) {
 									{{5.0, 5.0, 5.0, 1.0}, 4.0, {0.0, 255.0, 0.0}}
 								};
 	cudaMemcpyToSymbol(sphereArray, h_sphereArray, SPHERE_COUNT*sizeof(Sphere));
+	Analysis::end(0);
 
 	dim3 block(32, 32);
 	dim3 grid((IMAGE_WIDTH + block.x - 1) / block.x, (IMAGE_HEIGHT + block.y - 1) / block.y);
 
+	Analysis::begin();
+	printf("rendering ray traced image...\n");
 	colorFromRay<<<grid, block>>>(d_colorData);
 	cudaDeviceSynchronize();
+	printf("finished rendering\n");
+	Analysis::end(1);
 
+	Analysis::begin();
 	cudaMemcpy(h_colorData, d_colorData, IMAGE_WIDTH*IMAGE_HEIGHT*sizeof(Tuple), cudaMemcpyDeviceToHost);
 	cudaFree(d_colorData);
+	Analysis::end(2);
 
+	Analysis::begin();
 	const char* filename = argv[1];
 	writeColorDataToFile(filename, h_colorData);
 	printf("saved image as: [%s]\n", filename);
+	Analysis::end(3);
+
+	Analysis::printAll(IMAGE_WIDTH, IMAGE_HEIGHT);
 
 	cudaDeviceReset();
 	free(h_colorData);
