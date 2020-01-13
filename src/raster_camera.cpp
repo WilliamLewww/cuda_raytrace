80db0c35d17e7131d555be75dd224c2724d203bb
William Lew <WilliamMilesLew@gmail.com>
2020-01-12 21:15:18 -0800

    fixed: inversed camera to match ray-tracing world

--- src/raster_camera.cpp
+++ src/raster_camera.cpp
@@ -2,7 +2,7 @@
 
 RasterCamera::RasterCamera() {
   position = glm::vec3(0.0f, 0.0f, 0.0f);
-  up = glm::vec3(0.0f, 1.0f, 0.0f);
+  up = glm::vec3(0.0f, -1.0f, 0.0f);
 
   pitch = 0.0;
   yaw = 0.0;
@@ -28,8 +28,8 @@ float* RasterCamera::getProjectionMatrix() {
 
 void RasterCamera::update() {
   if (abs(Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_LEFT_X)) > 0.08) {
-    position.x += cos(yaw + (M_PI / 2)) * Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_LEFT_X) * 0.00005;
-    position.z += sin(yaw + (M_PI / 2)) * Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_LEFT_X) * 0.00005;
+    position.x += cos(yaw + (M_PI / 2)) * Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_LEFT_X) * -0.00005;
+    position.z += sin(yaw + (M_PI / 2)) * Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_LEFT_X) * -0.00005;
   }
   if (abs(Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_LEFT_Y)) > 0.08) {
     position.x += cos(yaw) * Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_LEFT_Y) * -0.00005;
@@ -37,11 +37,11 @@ void RasterCamera::update() {
   }
 
   if (abs(Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_RIGHT_X)) > 0.08) {
-    yaw += Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_RIGHT_X) * 0.0005;
+    yaw += Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_RIGHT_X) * -0.0005;
   }
 
   if (abs(Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_RIGHT_Y)) > 0.08) {
-    pitch += Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_RIGHT_Y) * -0.0005;
+    pitch += Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_RIGHT_Y) * 0.0005;
   }
 
   if (Input::checkGamepadAxis(GLFW_GAMEPAD_AXIS_LEFT_TRIGGER) > -0.92) {
